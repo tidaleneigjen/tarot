b61ac5ad0929c4e6dfdc7e1e183d33f7e13db1f6
jboss <justin.boss@gmail.com>
2025-07-01 20:52:16 -0400

    Cleaning up each view.

--- src/views/CardDetail.jsx
+++ src/views/CardDetail.jsx
@@ -1,6 +1,17 @@
 import TarotCard from '../components/TarotCard';
 
 export default function CardDetail({ card }) {
-  // Always upright in detail view
-  return <TarotCard card={card} isReversed={false} />;
+  if (!card) return null;
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <TarotCard
+        card={card}
+        isReversed={false} // or pass a prop if you track reversed here
+        showText={false}
+        showTitle={false}
+        className="max-h-[80vh] max-w-xs"
+      />
+    </div>
+  );
 }
